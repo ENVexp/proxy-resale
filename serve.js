baf0586bd6b277be51c4bedc76f177d11ee8a018
ceas <edu.alves1327@gmail.com>
2024-12-30 23:25:25 -0300

    teste

---
+++ serve.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/teste', async (req, res)=>{
+    try {
+        const response = await axios.get(
+            "http://tdck.cloud:443/series/Ronnyy/root@2424/679332.mp4",
+            {
+            responseType: 'stream'
+        });
+
+        res.setHeader('Content-Type', response.headers['content-type'] || 'application/octet-stream');
+        res.setHeader('Content-Length', response.headers['content-length'] || undefined);
+
+        response.data.pipe(res);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro', details: error.message });
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
